1bca0f01aa71f12b0dcb4d988168e5e1573f153f
Florent Morselli <florent+git@morselli.fr>
2015-10-02 14:39:54 +0200

    Bugs fixed

--- DependencyInjection/Compiler/AlgorithmCompilerPass.php
+++ DependencyInjection/Compiler/AlgorithmCompilerPass.php
@@ -17,83 +17,112 @@ use Symfony\Component\DependencyInjection\Reference;
 
 class AlgorithmCompilerPass implements CompilerPassInterface
 {
+    private function getSupportedKeywords()
+    {
+        return [
+            'required',
+            'recommended+',
+            'recommended',
+            'recommended-',
+            'optional',
+        ];
+    }
+
+    private function removeKeywordAlgorithms(array &$algorithms_enabled, array &$available_algorithms, array &$algorithms_loaded)
+    {
+        foreach ($algorithms_enabled as $key=>$algorithm) {
+            if ('!' === substr($algorithm, 0, 1)) {
+                $keyword = substr($algorithm, 1, strlen($algorithm)-1);
+                if (in_array($keyword, $this->getSupportedKeywords()) && array_key_exists($keyword, $available_algorithms)) {
+                    foreach ($available_algorithms[$keyword] as $algo) {
+                        if (array_key_exists($algorithm, $algo)) {
+                            unset($algorithms_loaded[$algorithm]);
+                        }
+                    }
+                    unset($algorithms_enabled[$key]);
+                }
+            }
+        }
+    }
+
+    private function removeAliasAlgorithms(array &$algorithms_enabled, array &$algorithms_loaded)
+    {
+        foreach ($algorithms_enabled as $key=>$algorithm) {
+            if ('!' === substr($algorithm, 0, 1)) {
+                if (true === array_key_exists(substr($algorithm, 1, strlen($algorithm)-1), $algorithms_loaded)) {
+                    unset($algorithms_loaded[substr($algorithm, 1, strlen($algorithm)-1)]);
+                }
+                unset($algorithms_enabled[$key]);
+            }
+        }
+    }
+
+    private function addAliasAlgorithms(array &$algorithms_enabled, array &$available_algorithms, array &$algorithms_loaded)
+    {
+        foreach ($algorithms_enabled as $key=>$algorithm) {
+            foreach ($available_algorithms as $algo) {
+                if (array_key_exists($algorithm, $algo)) {
+                    $algorithms_loaded[$algorithm] = $algo[$algorithm];
+                    unset($algorithms_enabled[$key]);
+                    break;
+                }
+            }
+        }
+    }
+
+    private function addKeywordAlgorithms(array &$algorithms_enabled, array &$available_algorithms, array &$algorithms_loaded)
+    {
+        foreach ($this->getSupportedKeywords() as $key=>$keyword) {
+            if (in_array($keyword, $algorithms_enabled) && array_key_exists($keyword, $available_algorithms)) {
+                $algorithms_loaded = array_merge(
+                    $algorithms_loaded,
+                    $available_algorithms[$keyword]
+                );
+                unset($algorithms_enabled[$key]);
+            }
+        }
+    }
+
+    private function processKeywords(array &$algorithms_enabled)
+    {
+        if (false !== ($pos = array_search('all', $algorithms_enabled))) {
+            unset($algorithms_enabled[$pos]);
+            $algorithms_enabled = array_merge(
+                $this->getSupportedKeywords(),
+                $algorithms_enabled
+            );
+        }
+    }
+
+    private function checkLoadedAlgorithms(array $algorithms_enabled, array $algorithms_loaded)
+    {
+        $diff = array_diff($algorithms_enabled, $algorithms_loaded);
+        if (!empty($diff)) {
+            throw new \InvalidArgumentException(sprintf('The following algorithms do not exist or can not be loaded: %s.', json_encode(array_values($diff))));
+        }
+    }
+
     public function process(ContainerBuilder $container)
     {
         if (!$container->hasDefinition('jose.algorithm_manager')) {
             return;
         }
 
-        $keywords = [
-            'required'     => 'getRequiredAlgorithms',
-            'recommended+' => 'getRecommendedPlusAlgorithms',
-            'recommended'  => 'getRecommendedAlgorithms',
-            'recommended-' => 'getRecommendedMinusAlgorithms',
-            'optional'     => 'getOptionalAlgorithms',
-            'all'          => 'getAllAlgorithms',
-        ];
-
-        $algorithms = [];
+        $available_algorithms = $this->getAvailableAlgorithms($container);
         $algorithms_enabled = $container->getParameter('jose.algorithms');
-        foreach ($keywords as $key => $method) {
-            if (in_array($key, $algorithms_enabled)) {
-                $algorithms = array_merge(
-                    $algorithms,
-                    $this->$method($container)
-                );
-                $array_keys = array_keys($algorithms_enabled, $key);
-                foreach ($array_keys as $array_key) {
-                    unset($algorithms_enabled[$array_key]);
-                }
-            }
-        }
+        $algorithms_loaded = [];
 
-        foreach ($algorithms as $algorithm) {
-            if ('!' === substr($algorithm, 0, 1)) {
-                if (true === array_key_exists(substr($algorithm, 1, strlen($algorithm)-1), $algorithms)) {
-                    unset($algorithms[substr($algorithm, 1, strlen($algorithm)-1)]);
-                }
-                unset($algorithms[$algorithm]);
-            }
-        }
-        
-        $algorithms = array_merge(
-            $algorithms,
-            $this->getAlgorithmsFromAliases($container, $algorithms_enabled)
-        );
-        $this->loadAlgorithms($container, $algorithms);
+        $this->processKeywords($algorithms_enabled);
+        $this->addKeywordAlgorithms($algorithms_enabled, $available_algorithms, $algorithms_loaded);
+        $this->addAliasAlgorithms($algorithms_enabled, $available_algorithms, $algorithms_loaded);
+        $this->removeKeywordAlgorithms($algorithms_enabled, $available_algorithms, $algorithms_loaded);
+        $this->removeAliasAlgorithms($algorithms_enabled, $algorithms_loaded);
+        $this->checkLoadedAlgorithms($algorithms_enabled, $algorithms_loaded);
+
+        $this->loadAlgorithms($container, $algorithms_loaded);
     }
 
-    private function getRequiredAlgorithms(ContainerBuilder $container)
-    {
-        return $this->getAlgorithmsFromRequirement($container, 'required');
-    }
-
-    private function getRecommendedPlusAlgorithms(ContainerBuilder $container)
-    {
-        return $this->getAlgorithmsFromRequirement($container, 'recommended+');
-    }
-
-    private function getRecommendedAlgorithms(ContainerBuilder $container)
-    {
-        return $this->getAlgorithmsFromRequirement($container, 'recommended');
-    }
-
-    private function getRecommendedMinusAlgorithms(ContainerBuilder $container)
-    {
-        return $this->getAlgorithmsFromRequirement($container, 'recommended-');
-    }
-
-    private function getOptionalAlgorithms(ContainerBuilder $container)
-    {
-        return $this->getAlgorithmsFromRequirement($container, 'optional');
-    }
-
-    private function getAllAlgorithms(ContainerBuilder $container)
-    {
-        return $this->getAlgorithmsFromRequirement($container, null);
-    }
-
-    private function getAlgorithmsFromRequirement(ContainerBuilder $container, $requirement = null)
+    private function getAvailableAlgorithms(ContainerBuilder $container)
     {
         $result = [];
         $taggedServices = $container->findTaggedServiceIds('jose_algorithm');
@@ -102,42 +131,12 @@ class AlgorithmCompilerPass implements CompilerPassInterface
                 if (!array_key_exists('alias', $attributes)) {
                     throw new \InvalidArgumentException(sprintf("The algorithm '%s' does not have any 'alias' attribute.", $id));
                 }
-                if (!array_key_exists('requirement', $attributes)) {
+                if (!array_key_exists('requirement', $attributes) || empty($attributes['requirement'])) {
                     throw new \InvalidArgumentException(sprintf("The algorithm '%s' does not have any 'requirement' attribute.", $id));
                 }
-                if (is_null($requirement) || $requirement === $attributes['requirement']) {
-                    $result[$attributes['alias']] = $id;
-                }
+                $result[$attributes['requirement']][$attributes['alias']] = $id;
             }
         }
-
-        return $result;
-    }
-
-    private function getAlgorithmsFromAliases(ContainerBuilder $container, array $aliases)
-    {
-        $loaded = ['!none'];
-        $result = [];
-        $taggedServices = $container->findTaggedServiceIds('jose_algorithm');
-        foreach ($taggedServices as $id => $tags) {
-            foreach ($tags as $attributes) {
-                if (!array_key_exists('alias', $attributes)) {
-                    throw new \InvalidArgumentException(sprintf("The algorithm '%s' does not have any 'alias' attribute.", $id));
-                }
-                if (!array_key_exists('requirement', $attributes)) {
-                    throw new \InvalidArgumentException(sprintf("The algorithm '%s' does not have any 'requirement' attribute.", $id));
-                }
-                if (in_array($attributes['alias'], $aliases)) {
-                    $loaded[] = $attributes['alias'];
-                    $result[$attributes['alias']] = $id;
-                }
-            }
-        }
-        $diff = array_diff($aliases, $loaded);
-        if (!empty($diff)) {
-            throw new \InvalidArgumentException(sprintf('The following algorithms do not exist or can not be loaded: %s.', json_encode(array_values($diff))));
-        }
-
         return $result;
     }
 
