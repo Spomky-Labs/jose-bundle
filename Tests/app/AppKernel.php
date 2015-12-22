bc0134ec294805d99fe8e56fdea09588e625e5d4
Florent Morselli <florent+git@morselli.fr>
2015-12-22 21:09:54 +0100

    Dependencies updated

--- Tests/app/AppKernel.php
+++ Tests/app/AppKernel.php
@@ -19,7 +19,6 @@ class AppKernel extends Kernel
         $bundles = [
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
 
             new SpomkyLabs\TestJoseBundle\TestJoseBundle(),
             new SpomkyLabs\JoseBundle\SpomkyLabsJoseBundle(),
