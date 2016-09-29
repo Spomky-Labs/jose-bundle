ac385ec0f709957a36ad4294f1e0e36290c7e9c9
Spomky <Spomky@users.noreply.github.com>
2016-09-29 08:36:31 +0200

    Update Keys.php

--- DependencyInjection/Source/JWKSetSource/Keys.php
+++ DependencyInjection/Source/JWKSetSource/Keys.php
@@ -11,13 +11,12 @@
 
 namespace SpomkyLabs\JoseBundle\DependencyInjection\Source\JWKSetSource;
 
-use SpomkyLabs\JoseBundle\DependencyInjection\Source\AbstractSource;
 use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 
-class Keys extends AbstractSource implements JWKSetSourceInterface
+class Keys extends AbstractJWKSetSource implements JWKSetSourceInterface
 {
     /**
      * {@inheritdoc}
