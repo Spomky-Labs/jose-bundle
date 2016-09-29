89661d67471814b8180e730d37a9de0935c541e9
Spomky <Spomky@users.noreply.github.com>
2016-09-29 08:49:14 +0200

    Update PublicJWKSet.php

--- DependencyInjection/Source/JWKSetSource/PublicJWKSet.php
+++ DependencyInjection/Source/JWKSetSource/PublicJWKSet.php
@@ -16,7 +16,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 
-class PublicJWKSet extends AbstractJWKSetSource implements JWKSetSourceInterface
+class PublicJWKSet extends AbstractJWKSetSource
 {
     /**
      * {@inheritdoc}
