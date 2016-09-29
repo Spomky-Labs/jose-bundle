75dbbf2dd294cbb53fb8335e2c8b2fd25fe6bf15
Spomky <Spomky@users.noreply.github.com>
2016-09-29 08:48:46 +0200

    Update JKU.php

--- DependencyInjection/Source/JWKSetSource/JKU.php
+++ DependencyInjection/Source/JWKSetSource/JKU.php
@@ -16,7 +16,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 
-class JKU extends AbstractJWKSetSource implements JWKSetSourceInterface
+class JKU extends AbstractJWKSetSource
 {
     /**
      * {@inheritdoc}
