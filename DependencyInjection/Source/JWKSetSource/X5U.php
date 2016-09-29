7b0e207499e1c91426af170a7189104f2e972428
Spomky <Spomky@users.noreply.github.com>
2016-09-29 08:49:27 +0200

    Update X5U.php

--- DependencyInjection/Source/JWKSetSource/X5U.php
+++ DependencyInjection/Source/JWKSetSource/X5U.php
@@ -16,7 +16,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 
-class X5U extends AbstractJWKSetSource implements JWKSetSourceInterface
+class X5U extends AbstractJWKSetSource
 {
     /**
      * {@inheritdoc}
