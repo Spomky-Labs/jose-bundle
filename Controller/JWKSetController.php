e3a5d5bd38c1ad2e4c26357f77929ad40986ee1f
Spomky <Spomky@users.noreply.github.com>
2016-09-29 08:23:48 +0200

    JWKSetController added

---
+++ Controller/JWKSetController.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2016 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace SpomkyLabs\JoseBundle\Controller;
+
+use Jose\Object\JWKSetInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+final class JWKSetController
+{
+    /**
+     * @var \Jose\Object\JWKSetInterface
+     */
+    private $jwkset;
+
+    /**
+     * JWKSetController constructor.
+     *
+     * @param \Jose\Object\JWKSetInterface $jwkset
+     */
+    public function __construct(JWKSetInterface $jwkset)
+    {
+        $this->jwkset = $jwkset;
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handleAction()
+    {
+        return new Response(json_encode($this->jwkset), Response::HTTP_OK, ['content-type' => 'application/jwk-set+json']);
+    }
+}
