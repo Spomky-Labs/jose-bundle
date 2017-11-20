15f8d274892adced15e1af20da8e993b34d632b8
Spomky <Spomky@users.noreply.github.com>
2017-11-20 15:51:52 +0100

    Bug fixed when a JKU source is created from the configuration file

--- DependencyInjection/Source/JWKSetSource/JKU.php
+++ DependencyInjection/Source/JWKSetSource/JKU.php
@@ -30,7 +30,7 @@ final class JKU extends DownloadedJWKSet
         $definition->setArguments([
             $config['url'],
             $config['is_secured'],
-            $config['cache'],
+            new Reference($config['cache']),
             $config['cache_ttl'],
             $config['is_https'],
         ]);
